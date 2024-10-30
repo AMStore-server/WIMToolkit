cb608f542a59c4ad11f840f6639c49b0102ac7f3
AMStore-server <aimodsitalia@proton.me>
2024-10-30 18:47:01 +0100

    up

---
+++ WIMtoolkitDowload.ps1
@@ -0,0 +1,21 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Output "WIMToolkit necessita di essere lanciato come Amministratore. Riavvio."
+    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm -Uri https://bit.ly/3HmQaDv | iex"
+    break
+}
+
+Remove-Item -Path "C:\WIMToolkit" -Recurse -Force | out-null
+
+# Change to the desktop directory
+cd "C:\"
+
+# Download the script from GitHub
+Invoke-WebRequest -Uri "https://github.com/MrNico98/WIMToolkit/archive/refs/heads/main.zip" -OutFile "WIMToolkit-main.zip"
+
+Expand-Archive -Path "WIMToolkit-main.zip" -DestinationPath "." -Force
+
+Move-Item -Path "WIMToolkit-main" -Destination "WIMToolkit" -Force
+
+Remove-Item -Path "WIMToolkit-main.zip" -Force
+
+Start-Process -FilePath ".\WIMToolkit\WIMToolkit.bat"
